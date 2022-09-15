5efc94695a806e7c5dae0dd05163c71a4f8f9e62
Stephane Ayotte <stephane_p_ayotte@hotmail.com>
2022-09-15 12:06:00 -0400

    Add projectile script. Destroy projectile after 1 second

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    private const float projectileLifetime = 1.0f;
+
+    private IEnumerator removeProjectile;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        removeProjectile = DestroyAfter(projectileLifetime);
+
+        StartCoroutine(removeProjectile);
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator DestroyAfter(float lifetime)
     {
-        
+        yield return new WaitForSeconds(lifetime);
+        Destroy(gameObject);
     }
+
 }
